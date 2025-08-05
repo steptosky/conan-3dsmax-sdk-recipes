85754300a7520823a8dccda1e36c7ceacddfb947
Alexander Khudekov <pancir3d@steptosky.com>
2025-08-05 19:23:24 +0300

    Add: New 3d max SDK

--- deploy.py
+++ deploy.py
@@ -46,7 +46,7 @@ import sys
 # Visual studio is needed for checking created packages.
 # You have to specify Visual Studio version you are using.
 # 14 - VS2015, 15 - VS2017, 16 - VS2019
-visual_studio_version = 16
+visual_studio_version = 17
 
 # ----------------------------------------------------------------------------------#
 # //////////////////////////////////////////////////////////////////////////////////#
@@ -58,13 +58,13 @@ visual_studio_version = 16
 # If it is enabled then after building the package
 # it will be uploaded to the specified conan remote repository
 # otherwise the package will be only in your local cache.
-upload = False
+upload = True
 
 # The remote alias. See "conan remote" in the conan docs.
-remote = 'steptosky'
+remote = 'stsff'
 
 # The remote user. See "conan user" in the conan docs.
-remote_user = ''
+remote_user = 'stsff'
 
 # The remote password. See "conan user" in the conan docs.
 remote_psw = ''
@@ -122,20 +122,26 @@ def do_package(pckg_name, sdk_version, sdk_dir):
 # You have to specify the correct path to 3DsMax sdk root folder.
 # If you don't want to use all of them just comment necessary lines.
 # [package name, 3d max SDK version, path to SDK]
-do_package('3DsMaxSdk9', '9.0', 'W:/Programs/Environments/env_3dmax/maxsdk_9')
-do_package('3DsMaxSdk2008', '10.0', 'W:/Programs/Environments/env_3dmax/maxsdk_2008')
-do_package('3DsMaxSdk2009', '11.0', 'W:/Programs/Environments/env_3dmax/maxsdk_2009')
-do_package('3DsMaxSdk2010', '12.0', 'W:/Programs/Environments/env_3dmax/maxsdk_2010')
-do_package('3DsMaxSdk2011', '13.0', 'W:/Programs/Environments/env_3dmax/maxsdk_2011')
-do_package('3DsMaxSdk2012', '14.0', 'W:/Programs/Environments/env_3dmax/maxsdk_2012')
-do_package('3DsMaxSdk2013', '15.0', 'W:/Programs/Environments/env_3dmax/maxsdk_2013')
-do_package('3DsMaxSdk2014', '16.0', 'W:/Programs/Environments/env_3dmax/maxsdk_2014')
-do_package('3DsMaxSdk2015', '17.0', 'W:/Programs/Environments/env_3dmax/maxsdk_2015')
-do_package('3DsMaxSdk2016', '18.0', 'W:/Programs/Environments/env_3dmax/maxsdk_2016')
-do_package('3DsMaxSdk2017', '19.0', 'W:/Programs/Environments/env_3dmax/maxsdk_2017')
-do_package('3DsMaxSdk2018', '20.0', 'W:/Programs/Environments/env_3dmax/maxsdk_2018')
-do_package('3DsMaxSdk2019', '21.0', 'W:/Programs/Environments/env_3dmax/maxsdk_2019')
-do_package('3DsMaxSdk2020', '22.0', 'W:/Programs/Environments/env_3dmax/maxsdk_2020')
+# do_package('3DsMaxSdk9', '9.0', 'W:/Programs/Environments/env_3dmax/maxsdk_9')
+# do_package('3DsMaxSdk2008', '10.0', 'W:/Programs/Environments/env_3dmax/maxsdk_2008')
+# do_package('3DsMaxSdk2009', '11.0', 'W:/Programs/Environments/env_3dmax/maxsdk_2009')
+# do_package('3DsMaxSdk2010', '12.0', 'W:/Programs/Environments/env_3dmax/maxsdk_2010')
+# do_package('3DsMaxSdk2011', '13.0', 'W:/Programs/Environments/env_3dmax/maxsdk_2011')
+# do_package('3DsMaxSdk2012', '14.0', 'W:/Programs/Environments/env_3dmax/maxsdk_2012')
+# do_package('3DsMaxSdk2013', '15.0', 'W:/Programs/Environments/env_3dmax/maxsdk_2013')
+# do_package('3DsMaxSdk2014', '16.0', 'W:/Programs/Environments/env_3dmax/maxsdk_2014')
+# do_package('3DsMaxSdk2015', '17.0', 'W:/Programs/Environments/env_3dmax/maxsdk_2015')
+# do_package('3DsMaxSdk2016', '18.0', 'W:/Programs/Environments/env_3dmax/maxsdk_2016')
+# do_package('3DsMaxSdk2017', '19.0', 'W:/Programs/Environments/env_3dmax/maxsdk_2017')
+# do_package('3DsMaxSdk2018', '20.0', 'W:/Programs/Environments/env_3dmax/maxsdk_2018')
+# do_package('3DsMaxSdk2019', '21.0', 'W:/Programs/Environments/env_3dmax/maxsdk_2019')
+# do_package('3DsMaxSdk2020', '22.0', 'W:/Programs/Environments/env_3dmax/maxsdk_2020')
+# do_package('3DsMaxSdk2021', '23.0', 'W:/Programs/Environments/env_3dmax/maxsdk_2021')
+# do_package('3DsMaxSdk2022', '24.0', 'W:/Programs/Environments/env_3dmax/maxsdk_2022')
+# do_package('3DsMaxSdk2023', '25.0', 'W:/Programs/Environments/env_3dmax/maxsdk_2023')
+# do_package('3DsMaxSdk2024', '26.0', 'W:/Programs/Environments/env_3dmax/maxsdk_2024')
+# do_package('3DsMaxSdk2025', '27.0', 'W:/Programs/Environments/env_3dmax/maxsdk_2025')
+# do_package('3DsMaxSdk2026', '28.0', 'W:/Programs/Environments/env_3dmax/maxsdk_2026')
 
 # ----------------------------------------------------------------------------------#
 # //////////////////////////////////////////////////////////////////////////////////#
